603a7e21ff9d53ef2c8e1491aa752eb349ff6d37
F3214434 Fernando Aparecido Ribas Franzim <fernando.franzim@bb.com.br>
2021-12-03 11:39:29 -0300

    Iniciado Comand

---
+++ src/br/com/alura/TestesPedidos.java
@@ -0,0 +1,21 @@
+package br.com.alura;
+
+import br.com.alura.orcamento.Orcamento;
+import br.com.alura.pedido.Pedido;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public class TestesPedidos {
+
+    public static void main(String[] args) {
+        Orcamento orcamento = new Orcamento(new BigDecimal("600"), 4);
+        String cliente = "Ana da Silva";
+        LocalDateTime data = LocalDateTime.now();
+
+        Pedido pedido = new Pedido(cliente, data, orcamento);
+
+        System.out.println("Salvar pedido no Banco de Dados!");
+        System.out.println("Enviar e-mail com dados do novo pedido!");
+    }
+}
